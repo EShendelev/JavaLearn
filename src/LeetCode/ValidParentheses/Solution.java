2a2054ddf88143b20ea3c3d4ef693d092c649021
EShendelev <sendeleve@yandex.ru>
2024-02-27 22:55:47 +0300

    add ValidParantheless

---
+++ src/LeetCode/ValidParentheses/Solution.java
@@ -0,0 +1,16 @@
+package LeetCode.ValidParentheses;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println((int) '(');
+        System.out.println((int) ')');
+        System.out.println((int) '{');
+        System.out.println((int) '}');
+        System.out.println((int) '[');
+        System.out.println((int) ']');
+    }
+
+    public boolean isValid(String s) {
+        return true;
+    }
+}
