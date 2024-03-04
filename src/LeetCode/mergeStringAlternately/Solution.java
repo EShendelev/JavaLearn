13bfb8fcdc75109176af07a48e79853a9d23ae82
EShendelev <sendeleve@yandex.ru>
2024-03-04 21:22:42 +0300

    1768

--- src/LeetCode/mergeStringAlternately/Solution.java
+++ src/LeetCode/mergeStringAlternately/Solution.java
@@ -1,5 +1,6 @@
 package LeetCode.mergeStringAlternately;
 
+//https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
 public class Solution {
     public static void main(String[] args) {
         System.out.println(mergeAlternately("abc", "d"));
@@ -7,15 +8,26 @@ public class Solution {
 
     public static String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
-        String res = "";
-        for (int i = 0; i < word1.length(); i++) {
+
+        int len = word1.length();
+
+        if (len > word2.length()) len = word2.length();
+
+        for (int i = 0; i < len; i++) {
             sb.append(word1.charAt(i));
             sb.append(word2.charAt(i));
         }
-        sb = word1.length() < word2.length() ? sb.append(word2.substring(word1.length())) :
-                sb.append(word1.substring(word2.length()));
-        res = sb.toString();
-        return res;
+
+        if (word1.length() < word2.length()) {
+            sb.append(word2.substring(word1.length()));
+        } else {
+            sb.append(word1.substring(word2.length()));
+        }
+
+        return sb.toString();
 
     }
 }
+
+
+
