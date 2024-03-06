c51de35a0786bfbe1f7d5976c81d9673682c9346
Evgeny Shendelev <63276570+EShendelev@users.noreply.github.com>
2024-03-06 08:50:20 +0000

    reverse-words-in-a-string

---
+++ src/LeetCode/reverseWordsInAString/Solution.java
@@ -0,0 +1,30 @@
+package LeetCode.reverseWordsInAString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(reverseWords("a good   example"));
+    }
+
+    public static String reverseWords(String s) {
+        //Получим массив строк
+        String[] strings = s.split(" ");
+        List<String> resList = new ArrayList<>();
+
+        //Проходим по массиву с конца
+        for (int i = strings.length - 1; i >= 0; i-- ) {
+            if (!strings[i].isBlank()) {
+                //у каждого слова убираем лишние пробелы с начала и конца
+                resList.add(strings[i].trim());
+            } else {
+                continue;
+            }
+        }
+        return String.join(" ", resList);
+    }
+}
