214904012e6a5ebe6564b316ad276ae8b56daa31
Evgeny Shendelev <63276570+EShendelev@users.noreply.github.com>
2024-03-06 12:51:53 +0000

    _

--- src/LeetCode/increasingTripletSubsequence/Solution.java
+++ src/LeetCode/increasingTripletSubsequence/Solution.java
@@ -1,5 +1,5 @@
 package LeetCode.increasingTripletSubsequence;
-
+//https://leetcode.com/problems/increasing-triplet-subsequence/?envType=study-plan-v2&envId=leetcode-75
 public class Solution {
     public static void main(String[] args) {
         System.out.println(increasingTriplet(new int[]{1,1,-2,6}));
